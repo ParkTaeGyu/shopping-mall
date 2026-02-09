0cf74bb530dd4c379a666d799a0c5617fe61abc0
ParkTaeKyu <taekyu2015@gmail.com>
2026-02-09 13:16:26 +0900

    [FIX] 웹 환경변수 로딩 경로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "assets/env/app.env");
   
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL'] ?? '',
